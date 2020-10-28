f3ec3879818e60cca63a29c268d56cac45979707
Yichao Yu <yyc1992@gmail.com>
2020-10-27 22:40:55 -0400

    [figure] Consistent x axis for linewidth fit

--- scripts/plot_pa_spectrum_v12_red_10.jl
+++ scripts/plot_pa_spectrum_v12_red_10.jl
@@ -55,24 +55,28 @@ const datas_cscs = [load_data(matopen(fd->read(fd, "names"), joinpath(expdir, na
                               NaCsData.select_single((-1, 2,), (4,)))
                     for names_file in names_files]
 
+for ds in [datas_cs, datas_na, datas, datas_cscs, datas_nana]
+    ds[1] = NaCsData.map_params((i1, i2, v)->(v - 496) * 1000, ds[1])
+end
+
 function model_lorentzian(x, p)
     p[1] .- p[2] ./ (1 .+ ((x .- p[3]) ./ (p[4] / 2)).^2)
 end
 
 const prefix = joinpath(@__DIR__, "../figures/pa_spectrum_v12_red_10")
 
-# fit_5 = fit_survival(model_lorentzian, datas[1][:, 1], [0.7, 0.35, 496.06, 0.1])
-fit_10 = fit_survival(model_lorentzian, datas[1][:, 2], [0.7, 0.35, 496.1, 0.2])
+# fit_5 = fit_survival(model_lorentzian, datas[1][:, 1], [0.7, 0.35, 60, 60])
+fit_10 = fit_survival(model_lorentzian, datas[1][:, 2], [0.7, 0.35, 100, 167], plot_lo=-20)
 
 figure()
 NaCsPlot.plot_survival_data(datas[1][:, 2], fmt="C0.")
 plot(fit_10.plotx, fit_10.ploty, "C0-")
-text(496.01, 0.71, "\$\\Gamma=$(fit_10.uncs[4] * 1000)\$ MHz", color="C0")
+text(10, 0.71, "\$\\Gamma=$(fit_10.uncs[4])\$ MHz", color="C0")
 grid()
 ylim([0.4, 0.8])
 yticks([0.4, 0.5, 0.6, 0.7, 0.8])
 # xlim([495.9, 496.34])
-xlabel("PA Frequency (306XXX GHz)")
+xlabel("PA Frequency (306496XXX MHz)")
 ylabel("Two-Body Survival")
 NaCsPlot.maybe_save("$(prefix)")
 
