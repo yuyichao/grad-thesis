ec346b3b43c7f6a1f78620b908a0837657b2aee0
Yichao Yu <yyc1992@gmail.com>
2020-11-21 22:54:55 -0500

    [raman-transfer] WIP: molecular scattering, add fitting for the difference

--- scripts/plot_raman_transfer_scatter_scaling.jl
+++ scripts/plot_raman_transfer_scatter_scaling.jl
@@ -27,6 +27,9 @@ const unc_Γ_mol1 = [0.045, 0.020, 0.023]
 const Γ_mol2 = [1.30, 0.395, 0.170]
 const unc_Γ_mol2 = [0.10, 0.041, 0.010]
 
+const Γ_mol_diff = Γ_mol2 .- Γ_mol1
+const unc_Γ_mol_diff = sqrt.(unc_Γ_mol1.^2 .+ unc_Γ_mol2.^2)
+
 function gen_model_power(pwr)
     return (x, p)->x.^pwr .* p[1]
 end
@@ -44,6 +47,9 @@ fit1_Γ_mol1 = fit_data(model_lin0, pwrs, Γ_mol1, unc_Γ_mol1, [0.05], plot_lo=
 fit1_Γ_mol2 = fit_data(model_lin0, pwrs, Γ_mol2, unc_Γ_mol2, [0.05], plot_lo=0)
 fit2_Γ_mol1 = fit_data(model_quad0, pwrs, Γ_mol1, unc_Γ_mol1, [0.01], plot_lo=0)
 fit2_Γ_mol2 = fit_data(model_quad0, pwrs, Γ_mol2, unc_Γ_mol2, [0.01], plot_lo=0)
+
+fit1_Γ_mol_diff = fit_data(model_lin0, pwrs, Γ_mol_diff, unc_Γ_mol_diff, [0.01], plot_lo=0)
+fit2_Γ_mol_diff = fit_data(model_quad0, pwrs, Γ_mol_diff, unc_Γ_mol_diff, [0.01], plot_lo=0)
 
 @show fit2_Γ_atom1.uncs # 2.93(17)\times10^{-2}
 @show fit2_Γ_atom2.uncs # 2.45(13)\times10^{-2}
@@ -63,8 +69,8 @@ errorbar(pwrs, Γ_atom2, unc_Γ_atom2, fmt="C1s")
 plot(fit1_Γ_atom2.plotx, fit1_Γ_atom2.ploty, "C1--")
 plot(fit2_Γ_atom2.plotx, fit2_Γ_atom2.ploty, "C1")
 xlim([2.4, 17])
-ylim([0.4, 35])
-legend(loc="upper left", fontsize="x-small")
+ylim([0.4, 40])
+legend(loc="lower right", fontsize="x-small")
 xscale("log")
 yscale("log")
 grid()
@@ -72,7 +78,7 @@ ax.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
 ax.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
 ax.set_xticks(3:17, minor=true)
 xticks([3, 6, 12], ["3", "6", "12"])
-yticks([1, 3, 10, 30], ["1", "3", "10", "30"])
+yticks([1, 3, 10, 30], ["1", "", "10", ""])
 xlabel("Tweezer Power (mW)")
 ylabel("\$\\Gamma_{\\mathrm{a}}~(\\mathrm{2\\pi\\cdot Hz})\$")
 NaCsPlot.maybe_save("$(prefix)_atom")
@@ -89,7 +95,7 @@ errorbar(pwrs, Γ_mol2, unc_Γ_mol2, fmt="C1s")
 plot(fit1_Γ_mol2.plotx, fit1_Γ_mol2.ploty, "C1")
 xlim([2.4, 17])
 ylim([0.1, 1.6])
-legend(loc="upper left", fontsize="x-small")
+legend(loc="lower right", fontsize="x-small")
 xscale("log")
 yscale("log")
 grid()
@@ -101,5 +107,28 @@ yticks([0.2, 0.4, 0.8, 1.6], ["0.2", "0.4", "0.8", "1.6"])
 xlabel("Tweezer Power (mW)")
 ylabel("\$\\Gamma_{\\mathrm{m}}~(\\mathrm{2\\pi\\cdot kHz})\$")
 NaCsPlot.maybe_save("$(prefix)_mol")
+
+figure()
+ax = gca()
+errorbar(pwrs, Γ_mol_diff, unc_Γ_mol_diff, fmt="C0o")
+plot(fit1_Γ_mol_diff.plotx, fit1_Γ_mol_diff.ploty, "C0--")
+plot(fit2_Γ_mol_diff.plotx, fit2_Γ_mol_diff.ploty, "C0")
+xlim([2.4, 17])
+ylim([0.008, 0.8])
+xscale("log")
+yscale("log")
+grid()
+text(3.8, 0.02,
+     ("\$\\Delta\\Gamma_{\\mathrm{m}}=P^2\\cdot2\\pi\\cdot$(fit2_Γ_mol_diff.uncs[1] .* 1000)" *
+      "~\\mathrm{Hz/mW^2}\$"), fontsize="small", color="C0")
+ax.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+ax.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+ax.set_xticks(3:17, minor=true)
+xticks([3, 6, 12], ["3", "6", "12"])
+yticks([0.01, 0.03, 0.1, 0.3], ["0.01", "", "0.1", ""])
+xlabel("Tweezer Power (mW)")
+ylabel("\$\\Delta\\Gamma_{\\mathrm{m}}~(\\mathrm{2\\pi\\cdot kHz})\$")
+gca().yaxis.set_label_coords(-0.09, 0.55)
+NaCsPlot.maybe_save("$(prefix)_mol_diff")
 
 NaCsPlot.maybe_show()
